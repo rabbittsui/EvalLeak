30c4e91e2ce6d8b3084b5575ff3c47386a153acf
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2018-07-17 10:32:00 +0800

    perf: improve python

--- src/evalleak/containment.py
+++ src/evalleak/containment.py
@@ -4,3 +4,7 @@ Exact and near duplicate checks compare whole records. They miss the case where
 a short evaluation item is embedded inside a longer training record: a question
 that appears verbatim as one paragraph of a longer document, for example. The
 evaluation item is fully present in the training data, so the model may have
+seen the answer, yet neither the digest nor the Jaccard of the whole records
+matches.
+
+This module detects that. For an ordered pair (short, long) it reports
