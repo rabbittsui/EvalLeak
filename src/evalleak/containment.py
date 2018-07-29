8a1d4d8488e48c5f5e8f67ab1d378228a928341d
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2018-07-29 16:34:00 +0800

    fix: correct python

--- src/evalleak/containment.py
+++ src/evalleak/containment.py
@@ -24,3 +24,7 @@ from dataclasses import dataclass
 from .normalise import NormaliseConfig, normalise
 
 
+@dataclass(frozen=True)
+class Containment:
+    """A containment finding: short_id sits inside long_id."""
+
