7823afe68b8ac3c1d861be87c2a544350f3c55a2
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2018-11-10 18:41:00 +0800

    feat: datasets

--- src/evalleak/overlap.py
+++ src/evalleak/overlap.py
@@ -162,3 +162,15 @@ def compare(
     by_split = {m.split: m for m in manifests}
     digests = {name: _digests(m, config) for name, m in by_split.items()}
     minhashes = {
+        name: _minhashes(m, config, k, num_perm) for name, m in by_split.items()
+    }
+    texts = {
+        name: {r.record_id: r.text for r in m.records} for name, m in by_split.items()
+    }
+
+    split_names = sorted(by_split)
+
+    # Intra-split exact duplication.
+    for name in split_names:
+        records = by_split[name].records
+        for i in range(len(records)):
