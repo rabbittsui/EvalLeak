48adec1adb7aed9a93b15b3dd5bbafdbda2ef0ef
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2018-12-03 16:12:00 +0800

    fix: correct cli

--- src/evalleak/overlap.py
+++ src/evalleak/overlap.py
@@ -186,3 +186,15 @@ def compare(
                             digest=digests[name][records[i].record_id],
                         )
                     )
+
+    # Cross-split comparisons over unordered pairs of splits.
+    for ai in range(len(split_names)):
+        for bi in range(ai + 1, len(split_names)):
+            name_a = split_names[ai]
+            name_b = split_names[bi]
+            recs_a = by_split[name_a].records
+            recs_b = by_split[name_b].records
+
+            exact_pairs: set[tuple[str, str]] = set()
+            for ra in recs_a:
+                for rb in recs_b:
