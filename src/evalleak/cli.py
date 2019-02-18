9fc23f0621074dfb709e91e91b069c8dc0c47917
ryotapatel2935 <309026439+ryotapatel2935@users.noreply.github.com>
2019-02-18 18:27:00 +0800

    fix: correct overlap

--- src/evalleak/cli.py
+++ src/evalleak/cli.py
@@ -69,3 +69,14 @@ def _add_common(parser: argparse.ArgumentParser) -> None:
     )
 
 
+def _config_from_args(args: argparse.Namespace) -> NormaliseConfig:
+    return NormaliseConfig(
+        whitespace=not args.no_whitespace,
+        case=not args.no_case,
+        punctuation=not args.no_punctuation,
+    )
+
+
+def _load_all(args: argparse.Namespace):
+    manifests = []
+    for path in args.manifests:
