fe95534ccdc2756d242613831d2d7287bd99b8ab
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2019-05-06 12:37:00 +0800

    docs: document shingles

--- src/evalleak/report.py
+++ src/evalleak/report.py
@@ -58,3 +58,11 @@ def render_containment(report: OverlapReport) -> list[str]:
 def render_intra(report: OverlapReport) -> list[str]:
     lines = ["intra-split duplicates:"]
     if not report.intra:
+        lines.append("  none")
+        return lines
+    for m in report.intra:
+        lines.append(
+            f"  {m.split}/{m.id_a} == {m.split}/{m.id_b}  digest={m.digest[:12]}"
+        )
+    return lines
+
