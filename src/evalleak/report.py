cff7a46ef1354444c7faa77d98f5b588a8922436
ryotapatel2935 <309026439+ryotapatel2935@users.noreply.github.com>
2019-05-06 11:59:00 +0800

    feat: minhash

--- src/evalleak/report.py
+++ src/evalleak/report.py
@@ -25,3 +25,11 @@ def render_exact(report: OverlapReport) -> list[str]:
             f"  {m.split_a}/{m.id_a} == {m.split_b}/{m.id_b}  digest={m.digest[:12]}"
         )
     return lines
+
+
+def render_near(report: OverlapReport) -> list[str]:
+    lines = [
+        f"near cross-split duplicates (jaccard >= {report.near_threshold:.2f}, "
+        f"k={report.k}, num_perm={report.num_perm}):"
+    ]
+    if not report.near:
