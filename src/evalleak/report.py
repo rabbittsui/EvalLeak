d9349e18b3716c0168ee93d233bd7fea25d2ce0e
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2019-05-07 10:07:00 +0800

    feat: add minhash

--- src/evalleak/report.py
+++ src/evalleak/report.py
@@ -74,3 +74,11 @@ def render_rates(report: OverlapReport) -> list[str]:
         return lines
     for r in report.pair_rates:
         lines.append(
+            f"  {r.source} -> {r.target}: {r.contaminated}/{r.total} "
+            f"= {_rate_pct(r.rate)}"
+        )
+    return lines
+
+
+def render_counts(report: OverlapReport) -> list[str]:
+    lines = ["split record counts:"]
