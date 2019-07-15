ea7cf69ab900973e9905dc0269a963155868c5c2
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2019-07-15 14:07:00 +0800

    perf: improve contamination

--- tests/test_evalleak.py
+++ tests/test_evalleak.py
@@ -179,3 +179,15 @@ class OverlapTests(unittest.TestCase):
     def test_deterministic_output(self):
         a = render_report(compare(self._load()))
         b = render_report(compare(self._load()))
+        self.assertEqual(a, b)
+
+    def test_no_findings_when_disjoint(self):
+        from evalleak.records import parse_manifest
+
+        m1 = parse_manifest("split: a\n\nid: x\ntext: apples grow on tall trees\n")
+        m2 = parse_manifest("split: b\n\nid: y\ntext: rockets travel through space\n")
+        report = compare([m1, m2])
+        self.assertFalse(report.has_findings())
+
+
+class CliTests(unittest.TestCase):
